7a71dc039aff6437932e82efdece5f8f2f2fc55e
Yogeshwar B <40350972+yogeshwar-b@users.noreply.github.com>
2024-04-17 16:11:43 +0530

    Evaluate-game (#3)
    * Game evaluation.
    
    * Added Box Move enum

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,26 +1,49 @@
-import { useEffect, useReducer } from 'react'
+import { useEffect, useReducer, useRef, useState } from 'react'
+import { EvaluateGame } from '../utils/EvaluateGame'
+import { BoxMove } from '../../models/boardenums'
 
 const numberofrows: number = 3
 const boxsize: string = '100px'
 
 const GameBoard = () => {
+  const [winner, SetWinner] = useState('')
   return (
     <div>
-      <NxNBoard n={numberofrows}></NxNBoard>
+      {winner == '' ? <></> : <div>WINNER - {winner}</div>}
+      <NxNBoard n={numberofrows} handleWinner={SetWinner}></NxNBoard>
     </div>
   )
 }
 
 interface boardprops {
   n: number
+  handleWinner: React.Dispatch<React.SetStateAction<string>>
 }
 const NxNBoard = (props: boardprops) => {
-  let initialBoxStates: boolean[] = Array(props.n ** 2).fill(false)
+  const initialBoxStates: BoxMove[] = Array(props.n ** 2).fill(BoxMove.None)
   const [boxesState, dispatch] = useReducer(BoxesReducer, initialBoxStates)
+  let PlayerRef = useRef(true) //True - P1 , False - P2
+
   function handleBoxChange(boxnumber: string) {
-    dispatch({ type: 'changed', boxnumber: boxnumber })
+    dispatch({
+      type: 'changed',
+      boxnumber: boxnumber,
+      PlayerRef: PlayerRef,
+      handleWinner: props.handleWinner
+    })
   }
-  let idx2: number = 0
+  function handleBoxReset() {
+    dispatch({
+      type: 'reset',
+      BoxState: initialBoxStates
+    })
+  }
+
+  let idx2: number = -1
+
+  useEffect(() => {
+    PlayerRef.current = !PlayerRef.current
+  })
   return (
     /**
      * @todo - change to css file
@@ -28,17 +51,18 @@ const NxNBoard = (props: boardprops) => {
     <div
       style={{
         display: 'grid',
-        gridTemplateColumns: 'repeat(3,' + boxsize + ')',
+        gridTemplateColumns: 'repeat(' + numberofrows + ',' + boxsize + ')',
         justifyItems: 'center'
       }}
     >
-      {boxesState.map((st: boolean) => {
+      {boxesState.map((st: BoxMove) => {
         idx2++
         return (
           <PieceButton
             boxnumber={String(idx2)}
             boxChanged={handleBoxChange}
             boxstate={st}
+            key={String(idx2)}
           ></PieceButton>
         )
       })}
@@ -49,13 +73,9 @@ const NxNBoard = (props: boardprops) => {
 interface piecebuttonprops {
   boxnumber: string
   boxChanged: (boxnumber: string) => void
-  boxstate: boolean
+  boxstate: BoxMove
 }
 const PieceButton = (props: piecebuttonprops) => {
-  // const [btnclick, changebtnclick] = useState(false)
-  useEffect(() => {
-    // console.log('rendered ' + props.boxnumber)
-  })
   return (
     <button
       type='button'
@@ -65,12 +85,14 @@ const PieceButton = (props: piecebuttonprops) => {
       style={{ display: 'inline', height: boxsize, width: boxsize }}
       name={String(props.boxnumber)}
       onClick={() => {
-        console.log('changed ' + String(props.boxnumber))
         props.boxChanged(String(props.boxnumber))
-        // changebtnclick(!btnclick)
       }}
     >
-      {props.boxstate ? 'X' : 'O'}
+      {props.boxstate == BoxMove.None
+        ? '-'
+        : props.boxstate == BoxMove.P1
+        ? 'O'
+        : 'X'}
     </button>
   )
 }
@@ -78,20 +100,40 @@ const PieceButton = (props: piecebuttonprops) => {
 interface action {
   type: string
   boxnumber: string
+  PlayerRef: React.MutableRefObject<boolean>
+  BoxState: BoxMove[]
+  handleWinner: React.Dispatch<React.SetStateAction<string>>
 }
-function BoxesReducer(boxStates: boolean[], action: Partial<action>) {
+function BoxesReducer(boxStates: BoxMove[], action: Partial<action>) {
   switch (action.type) {
     case 'changed': {
-      // console.log('changed called for ' + action.boxnumber)
-      let i = 0
-      return boxStates.map((st: boolean) => {
+      let i = -1
+      let temp: BoxMove[] = boxStates.map((st: BoxMove) => {
         i++
-        return String(i) == action.boxnumber ? !st : st
+        return String(i) == action.boxnumber
+          ? action.PlayerRef?.current
+            ? BoxMove.P1
+            : BoxMove.P2
+          : st
       })
+      if (EvaluateGame(temp, Number(action.boxnumber))) {
+        console.log(
+          (action.PlayerRef?.current ? 'Player O ' : 'Player X ') + ' won'
+        )
+        if (action.handleWinner)
+          action.PlayerRef?.current
+            ? action.handleWinner('Player O')
+            : action.handleWinner('Player X')
+      }
+      return temp
+    }
+    case 'reset': {
+      if (action.handleWinner) action.handleWinner('')
+      return action.BoxState ? action.BoxState : boxStates
     }
     default: {
       console.log('default called')
-      return [...boxStates]
+      return boxStates
     }
   }
 }
---
+++ src/utils/EvaluateGame.tsx
@@ -0,0 +1,66 @@
+import { BoxMove } from '../../models/boardenums'
+
+var match = ''
+var side = 0
+export function EvaluateGame(BoxStates: BoxMove[], idx: number): boolean {
+  side = Math.sqrt(BoxStates.length)
+  //console.log(side)
+  match = String(BoxStates[idx])
+  let i = Math.floor(idx / side) //Row number
+  let j = idx % side //Col number
+  if (i == j || i == side - 1 - j) {
+    //box on diagonal
+    if (CheckCorner0(BoxStates) || CheckCornerN(BoxStates)) {
+      //console.log('corner matched')
+      return true
+    }
+  }
+  return CheckRows(idx, BoxStates) || CheckCols(idx, BoxStates) ? true : false
+}
+
+function CheckRows(idx: number, BoxStates: BoxMove[]): boolean {
+  //console.log('row check')
+  for (let j = 0; j < side; j++) {
+    //console.log(Math.floor(idx / side) + j)
+    if (String(BoxStates[Math.floor(idx / side) * side + j]) != String(match)) {
+      return false
+    }
+  }
+  //console.log('row matched')
+
+  return true
+}
+
+function CheckCols(idx: number, BoxStates: BoxMove[]): boolean {
+  //console.log('col check ' + side)
+  for (let i = 0; i < side; i++) {
+    //console.log((idx % side) + side * i)
+    if (String(BoxStates[(idx % side) + side * i]) != String(match)) {
+      return false
+    }
+  }
+  //console.log('col matched')
+
+  return true
+}
+
+function CheckCorner0(BoxStates: BoxMove[]): boolean {
+  //Check ↘ diagonal
+  for (let i = 0; i < side; i++) {
+    if (String(BoxStates[i * side + i]) !== String(match)) {
+      return false
+    }
+  }
+  //console.log('corner1')
+  return true
+}
+function CheckCornerN(BoxStates: BoxMove[]): boolean {
+  //Check ↙ diagonal
+  for (let i = 0; i < side; i++) {
+    if (String(BoxStates[i * side + (side - 1 - i)]) !== String(match)) {
+      return false
+    }
+  }
+  //console.log('corner2')
+  return true
+}
